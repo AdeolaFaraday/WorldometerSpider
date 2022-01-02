bb97f54566b9a6ca737684426b9e9135c43c15b3
adeolapunch <apanisile@punch.cool>
2022-01-02 06:15:37 -0800

    working with links with scrapy

--- worldometers/spiders/countries.py
+++ worldometers/spiders/countries.py
@@ -4,14 +4,18 @@ import scrapy
 
 class CountriesSpider(scrapy.Spider):
     name = 'countries'
-    allowed_domains = ['www.worldometers.info/']
-    start_urls = ['https://www.worldometers.info/world-population/population-by-country/']
+    allowed_domains = ['www.worldometers.info']
+    start_urls = [
+        'https://www.worldometers.info/world-population/population-by-country/']
 
     def parse(self, response):
-        title = response.xpath('//h1/text()').get()
-        countries = response.xpath('//td/a/text()').getall()
+        countries = response.xpath('//td/a')
 
-        yield {
-            'title': title,
-            'countries': countries
-        }
+        for country in countries:
+            name = country.xpath(".//text()").get()
+            link = country.xpath(".//@href").get()
+
+            # absolute_url = f"https://www.worldometers.info{link}"
+            # absolute_url = response.urljoin(link)
+
+            yield response.follow(url=link)
